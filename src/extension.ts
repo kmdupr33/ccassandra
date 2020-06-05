d15684f1b41d4cb4ad9884fa413c1531647c50e0
Matt Dupree <kmdupr33@gmail.com>
2020-06-04 21:33:29 -0400

    remove unneeded import

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import { FileEffortMap } from "./change-score";
 import { StatusBarItem } from "./status-bar-item";
 import { Workspace } from "./workspace";
 
